8267adbb28d8702f6289319e6f0729ed6931cd7c
SATYA0110 <100819510+SATYA0110@users.noreply.github.com>
2026-09-15 19:08:00 +0500

    Update backend.tf

--- GKE_Infra/environment/prod/backend.tf
+++ GKE_Infra/environment/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.5.0"
     backend "gcs" {
-        bucket = "prod-tfstate-bucket"
+        bucket = "prod-tfstate-bucket01"
         prefix = "terraform/state/production"
 
     }
