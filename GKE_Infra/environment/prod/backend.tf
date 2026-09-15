11cb915e5660e97a29c466fbcbaa1bad94d0302a
SATYA0110 <ssnitesh7777@gmail.com>
2026-09-15 19:06:36 +0500

    newdd

--- GKE_Infra/environment/prod/backend.tf
+++ GKE_Infra/environment/prod/backend.tf
@@ -1,6 +1,9 @@
+
+
 terraform {
+  required_version = ">= 1.5.0"
     backend "gcs" {
-        bucket = "prod-tfstate-bucket"
+        bucket = "prod-tfstate-bucket01"
         prefix = "terraform/state/production"
 
     }
