11ca17302e3a04ac413e7fc26642715167ad2f74
SATYA0110 <100819510+SATYA0110@users.noreply.github.com>
2026-09-17 01:15:10 +0500

    Update provider.tf

--- GKE_Infra/environment/prod/provider.tf
+++ GKE_Infra/environment/prod/provider.tf
@@ -11,4 +11,5 @@ terraform {
 provider "google" {
     project = var.project_id
     region = var.region
+    impersonate_service_account = "id-19589081310-compute-develop@project-b072ca81-0008-42cb-81c.iam.gserviceaccount.com"
 }
