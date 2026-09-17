acf051aa78ef1ada6a54422cf0db495e35ece8a0
SATYA0110 <100819510+SATYA0110@users.noreply.github.com>
2026-09-17 11:32:59 +0500

    Update main.tf

--- GKE_Infra/modules/node_pool/main.tf
+++ GKE_Infra/modules/node_pool/main.tf
@@ -26,6 +26,13 @@ resource "google_container_node_pool" "gke_node" {
     shielded_instance_config {
       enable_secure_boot          = true
       enable_integrity_monitoring = true
+
+    service_account = var.node_service_account
+
+    # ✅ FIXED: Changed to the required full URL string literal
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
     }
 
     metadata = {
